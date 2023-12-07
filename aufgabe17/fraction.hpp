cc42112a794a9086a6eeeaa9c0ec8cb3e560930d
Colfrozenliquid <colfrozenliquid@gmail.com>
2023-12-07 12:39:10 +0100

    init commit prakt5

---
+++ aufgabe17/fraction.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+class Fraction {
+    public:
+        Fraction(int n=0, int d=1);
+        ~Fraction();
+        Fraction(const Fraction &f);
+
+        Fraction& operator=(const Fraction &f);
+        Fraction add(const Fraction&);
+        Fraction subtract(const Fraction&);
+        Fraction multiply(const Fraction&);
+        Fraction divide(const Fraction&);
+        void read();
+        void print();
+    
+    private:
+        int numerator;
+        int denominator;
+
+        int gcd(int, int);
+        void cancel();
+        void format();
+};
